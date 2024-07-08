e6ac61a777e83bcfd9c8fb10f6a1d7e68371a1bf
anumktk <anumshahana29@gmail.com>
2024-07-08 17:32:11 +0500

    update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { useState } from "react";
+import { v4 as uuidv4 } from 'uuid';
 
 function App() {
+  const [task,setTask] = useState("");
+  const [taskList, settaskList] = useState([]);
+  function handleAdd() {
+    const newList = taskList.concat({ id: uuidv4() , task });
+    settaskList(newList);
+  }
+  function handleDelete() {
+    const newList = [];
+    settaskList(newList);
+  }
+
   return (
+    
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1> Todo List </h1>
+      <input value={task} type="text" placeholder="Add your task" onChange={(e)=> {setTask(e.target.value)}}/>
+      <button onClick={handleAdd}>Add Task</button>      
+      <button onClick={handleDelete}>Delete All Tasks</button><br/><br/>
+      <h3> My Todo List... </h3>
+      <ul>
+      {taskList.map((item) => (
+        <li key={item.id}>{item.task}</li>
+      ))}
+    </ul>
+
     </div>
   );
 }
